7167be168382ad977800aad822f7e64cc8feff66
velinolis <velinolis@gmail.com>
2021-08-13 11:33:26 +0200

    Update CustomUse.ino

--- CustomUse.ino
+++ CustomUse.ino
@@ -2,9 +2,13 @@
 AnalogtoDigital AD;
 
 void setup() {
-  AD.Convert_All();
+  //AD.Convert_All();
   //AD.Select_Pin(14, 0, 1, 1);
   //AD.N_Pins(2);
+  AD.ADC_PortC();
 }
 
-void loop() {}
+void loop() {
+  //PINC = bit (0);
+  //PINC = bit (1);
+}
